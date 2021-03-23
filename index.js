a13df080f1752631b049b2eb180ceb1e9c0f7af0
caitlin <caitlin.wink@gmail.com>
2021-03-23 13:07:21 -0700

    commit tree 32f183239735afb36eb2e331581300467fa23259

---
+++ index.js
@@ -0,0 +1,15 @@
+function spy(){
+    console.log("Big brother is nanacat");
+    }
+
+function receivesAFunction(callback){
+            return spy();
+        }
+
+
+
+        function returnsANamedFunction(){
+    }
+
+function returnsAnAnonymousFunction(){
+}
